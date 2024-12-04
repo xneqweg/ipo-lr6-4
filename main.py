010bc8cc497f8003ddc933d165c6a8c73e8ed2b6
anna <141744002+xneqweg@users.noreply.github.com>
2024-12-04 15:30:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 podstr = input("Введите строку для поиска: ") #Запрос строки от пользователя
 
-with open('D:\profiles\PopkovA_81\Desktop\lr6', 'r', encoding='utf-8') as file: #Открываем файл в режиме чтения 
+with open('text.txt', 'r', encoding='utf-8') as file: #Открываем файл в режиме чтения 
     strk = file.readlines() #Считывание строк
     
 lines = [] #Создание списка
@@ -21,6 +21,6 @@ for line in lines: #Цикл
     print(line) #Вывод на экран
     
 sorted_lines = sorted(lines, key = len) #Сортируем список по длине строк
-
+print("Сортированные строки: ") #Вывод на экран
 for line in sorted_lines: #Цикл отсортированных строк
-    print(f"Сортированные строки: {line}") #Вывод на экран
+    print(line) #Вывод на экран
